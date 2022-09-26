477ebcd0b2c33b6dfcaabfeb75ce8de7b26da26f
陈宇通 <feyucccc@gmail.com>
2022-09-26 14:44:55 +0800

    fix：config error

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { props2table } from '../src/index'
 
 export default defineConfig({
-  base: '/vitepress-plugin-props2table/',
   server: {
     open: true,
     hmr: true,
