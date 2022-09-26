91841170d84b41eeeec912dacb28c086cbed60b2
陈宇通 <feyucccc@gmail.com>
2022-09-26 14:20:48 +0800

    fix： ci path

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { props2table } from '../src/index'
 
 export default defineConfig({
-  base: '/props2table/',
+  base: '/vitepress-plugin-props2table/',
   server: {
     open: true,
     hmr: true,
