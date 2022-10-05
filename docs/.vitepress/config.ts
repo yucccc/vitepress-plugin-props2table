ffa4b15e5837508445a0482822f0b9190baefba1
陈宇通 <feyucccc@gmail.com>
2022-10-05 15:38:40 +0800

    docs：add github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
+  title: 'props2table',
   base: '/vitepress-plugin-props2table/',
+  themeConfig: {
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/yucccc/vitepress-plugin-props2table' }
+    ],
+  }
 })
